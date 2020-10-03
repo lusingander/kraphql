78a204a4564f7e999adbe353da3a1af8e26d702b
Kyosuke Fujimoto <kyoro.f@gmail.com>
2020-10-03 16:36:07 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.3.72"
+}
+
+group = "com.github.lusingander.kraphql"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
