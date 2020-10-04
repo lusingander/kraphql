7d697eae6004e6b42207fdc0e1ec7b229e10a76f
Kyosuke Fujimoto <kyoro.f@gmail.com>
2020-10-04 22:36:59 +0900

    Generate tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "1.3.72"
-    application
+    java
 }
 
 group = "com.github.lusingander.kraphql"
@@ -8,13 +8,37 @@ version = "0.0.1"
 
 repositories {
     mavenCentral()
+    maven("https://kotlin.bintray.com/kotlinx")
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("com.graphql-java:graphql-java:2020-10-01T21-06-37-558aeaa")
+    implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3")
 }
 
-application {
-    mainClassName = "com.github.lusingander.kraphql.KraphQLKt"
+tasks {
+
+    val sdlDir = file("src/test/resources/sdl/")
+    val sdlFiles = sdlDir.listFiles()
+    sdlFiles.forEach {
+        register("generate-${it.name}", JavaExec::class.java) {
+            main = "com.github.lusingander.kraphql.KraphQLKt"
+            classpath = sourceSets["main"].runtimeClasspath
+            group = "generate"
+            args = listOf(
+                "--input",
+                it.absolutePath,
+                "--output",
+                "./src/test/kotlin/com/github/lusingander/kraphql/gen/${it.nameWithoutExtension}.kt"
+            )
+        }
+    }
+
+    register("generate") {
+        group = "build"
+        sdlFiles.forEach {
+            dependsOn("generate-${it.name}")
+        }
+    }
 }
--- src/main/kotlin/com/github/lusingander/kraphql/KraphQL.kt
+++ src/main/kotlin/com/github/lusingander/kraphql/KraphQL.kt
@@ -1,19 +1,24 @@
 package com.github.lusingander.kraphql
 
 import com.github.lusingander.kraphql.dsl.DslBuilder
-import com.github.lusingander.kraphql.sdl.SdlLoader
 import com.github.lusingander.kraphql.sdl.SdlParser
+import kotlinx.cli.ArgParser
+import kotlinx.cli.ArgType
+import kotlinx.cli.required
+import java.io.File
 import java.io.PrintWriter
 
-fun main() {
-    val sdls = SdlLoader().load()
-    val builder = DslBuilder(PrintWriter(System.out, true))
+fun main(args: Array<String>) {
+    val argParser = ArgParser("kraphql")
+    val input by argParser.option(ArgType.String, shortName = "i", description = "Input SDL file path").required()
+    val output by argParser.option(ArgType.String, shortName = "o", description = "Output .kt file path").required()
+    argParser.parse(args)
 
-    sdls.map {
-        SdlParser(it)
-    }.map {
-        it.parse()
-    }.forEach {
-        builder.build(it)
+    val sdl = File(input).readText()
+    val parsed = SdlParser(sdl).parse()
+
+    File(output).printWriter().use { writer ->
+        val builder = DslBuilder(writer)
+        builder.build(parsed)
     }
 }
